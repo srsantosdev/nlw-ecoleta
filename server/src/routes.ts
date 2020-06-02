b91915c3985d1b49182bdc4a3e1ec7a509caaeb9
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-06-02 14:44:19 -0300

    Controllers de Itens e de Pontos de Coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,17 @@
 import express from "express";
 
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
 const routes = express.Router();
+
+const Points = new PointsController();
+const Items = new ItemsController();
+
+routes.get("/items", Items.index);
+
+routes.get('/points/:id', Points.show)
+routes.get("/points", Points.index);
+routes.post("/points", Points.create);
 
 export default routes;
