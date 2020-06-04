6e3b329ba51412bdc85e13ae42aed6981b42c7bc
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-06-04 14:09:38 -0300

    Configuração da página 'Home'

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Feather as Icon } from "@expo/vector-icons";
+import {
+  Container,
+  Logo,
+  Main,
+  Title,
+  Description,
+  Footer,
+  Button,
+  ButtonIcon,
+  ButtonText,
+} from "./styles";
+
+const Home: React.FC = () => {
+  return (
+    <Container
+      source={require("./../../assets/home-background.png")}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <Main>
+        <Logo source={require("./../../assets/logo.png")} />
+        <Title>Seu marketplace de coleta de resíduos.</Title>
+        <Description>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Description>
+      </Main>
+
+      <Footer>
+        <Button onPress={() => {}}>
+          <ButtonIcon>
+            <Icon name="arrow-right" color="#fff" size={24} />
+          </ButtonIcon>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Footer>
+    </Container>
+  );
+};
+
+export default Home;
