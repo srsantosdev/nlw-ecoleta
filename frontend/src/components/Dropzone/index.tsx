f2bf3944bdb1efcc2ad4d498022b3edc928e8060
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-06-06 00:37:12 -0300

    Upload de imagens web

--- frontend/src/components/Dropzone/index.tsx
+++ frontend/src/components/Dropzone/index.tsx
@@ -3,14 +3,23 @@ import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { FiUpload } from "react-icons/fi";
 
-const Dropzone: React.FC = () => {
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
   const [selectedFileUrl, setSelectedFileUrl] = useState<string>("");
 
-  const onDrop = useCallback((acceptedFiles) => {
-    const file = acceptedFiles[0];
-    const fileUrl = URL.createObjectURL(file);
-    setSelectedFileUrl(fileUrl);
-  }, []);
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      const fileUrl = URL.createObjectURL(file);
+      setSelectedFileUrl(fileUrl);
+
+      onFileUploaded(file);
+    },
+    [onFileUploaded]
+  );
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -53,6 +53,7 @@ const CreatePoint: React.FC = () => {
   const [selectedUF, setSelectedUF] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -127,16 +128,20 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     api
       .post("points", data)
@@ -170,7 +175,7 @@ const CreatePoint: React.FC = () => {
           Cadastro do <br /> ponto de coleta.
         </h1>
 
-        <Dropzone />
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
